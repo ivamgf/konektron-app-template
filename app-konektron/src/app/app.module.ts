0daa9383d61562af46f21b3296a13a65775593b5
ivamgf <programador.ivam@gmail.com>
2023-02-06 15:29:21 -0300

    settings routing modules and add new components

--- app-konektron/src/app/app.module.ts
+++ app-konektron/src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './nav/header/header.component';
+import { FooterComponent } from './nav/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { SigninComponent } from './sign/signin/signin.component';
+import { SignupComponent } from './sign/signup/signup.component';
+import { DashComponent } from './dashboard/dash/dash.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { SecurityComponent } from './dashboard/security/security.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TermsComponent,
+    RecoveryComponent,
+    SigninComponent,
+    SignupComponent,
+    DashComponent,
+    ProfileComponent,
+    ServicesComponent,
+    ProductsComponent,
+    SecurityComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
